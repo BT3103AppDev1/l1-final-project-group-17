cd77baad3a750f76693098ea811156a553325063
Megan Poh <e0774405@u.nus.edu>
2023-03-14 22:17:54 +0800

    title for our website added

---
+++ src/components/TheHomepage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Welcome to the NUS Library Seats Booker</h1>
+</template>
+
+<script>
+
+export default {
+
+    }
+</script>
+
+<style scoped>
+h1 {
+    background-color: rgb(129, 184, 99);
+    text-align:center;
+}
+
+</style>

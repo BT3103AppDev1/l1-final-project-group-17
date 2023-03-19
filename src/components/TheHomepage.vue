c7939a6ec006e2c3a79921504929606e6ca6009e
hyc-g <haoyaochen00@gmail.com>
2023-03-19 14:10:15 +0800

    add routing

--- src/components/TheHomepage.vue
+++ src/components/TheHomepage.vue
@@ -3,16 +3,14 @@
 </template>
 
 <script>
-
 export default {
 
-    }
+}
 </script>
 
 <style scoped>
 h1 {
     background-color: rgb(129, 184, 99);
-    text-align:center;
+    text-align: center;
 }
-
 </style>

b297452a22b71c3da62192aa0952d3a180b89441
hyc-g <haoyaochen00@gmail.com>
2023-03-19 14:14:34 +0800

    routing to booking

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Booking from '@/views/Booking.vue'
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/Booking',
+        name: 'Booking',
+        component: Booking
     }
 ]
 

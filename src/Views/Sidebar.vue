c91d407570bd772183c91c1422c02bde638af77d
Megan Poh <e0774405@u.nus.edu>
2023-03-29 01:20:05 +0800

    updated Sidebar view

--- src/Views/Sidebar.vue
+++ src/Views/Sidebar.vue
@@ -1,48 +1,41 @@
 <template>
-    <div id = "main">
-        <TheSidebarVue />
+    <TheSidebarVue />
+    <div :style="{ 'margin-left': sidebarWidth }">
+      <router-view />
     </div>
-</template>
+  </template>
 
 <script>
-    import TheSidebarVue from '@/components/TheSidebar.vue'
+
+    import TheSidebarVue from '@/components/sidebar/TheSidebar.vue'
+    import { sidebarWidth } from '@/components/sidebar/state'
         export default {
           name: 'App',
           components:{
               TheSidebarVue
           },
+          setup() {
+    return { sidebarWidth }
+  }
         }
 </script>
 
-<style lang="scss">
-:root {
- --sidebar-width: 300px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-* {
- margin: 0;
- padding: 0;
- box-sizing: border-box;
- font-family: 'Fira sans', sans-serif;
+#nav {
+  padding: 30px;
 }
-body {
- background: var(--light);
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-button {
- cursor: pointer;
- appearance: none;
- border: none;
- outline: none;
- background: none;
-}
-.app {
- display: flex;
- main {
-  flex: 1 1 0;
-  padding: 2rem;
-  
-  @media (max-width: 768px) {
-   padding-left: 6rem;
-  }
- }
+#nav a.router-link-exact-active {
+  color: #efc700;
 }
 </style>

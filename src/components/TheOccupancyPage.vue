8a2f77c107da2d9bfd5537bf9a84cac007d5460d
jwhhhh <hor.jiaweii@gmail.com>
2023-04-06 01:40:12 +0800

    updated occupancy code

--- src/components/TheOccupancyPage.vue
+++ src/components/TheOccupancyPage.vue
@@ -1,46 +1,61 @@
 <template>
-    <div class = "occupancy">
-        <h2 id="Current occupancy"> CLB current occupancy: {{ clbOcc }}% </h2>
+    <div id = "occupancy1">
+        <table id = "occupancytable">
+            <tr>
+                <th></th>
+                <th>Central Library</th>
+            </tr>
+
+            <!--link to database and get occupancy. refer to profit display vue-->
+            <tr>
+                <td>Library occupancy</td>
+                <td> {{ clbOcc }}%</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-import firebaseApp from '../firebase.js';
-import { getFirestore } from 'firebase/firestore';
-import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+    import firebaseApp from '../firebase.js';
+    import { getFirestore } from 'firebase/firestore';
+    import { collection, getDoc, getDocs, doc, deleteDoc } from 'firebase/firestore';
+    import {getAuth, onAuthStateChanged} from "firebase/auth";
 
-const db = getFirestore(firebaseApp);
-let today = new Date().toLocaleDateString().split('')[0];
-let fulltime = new Date().toLocaleDateString().split('')[1];
-let time = fulltime.split(':')[0] + '00';
+    const db = getFirestore(firebaseApp)
 
-//get data from occupancy collection based on time 
+    export default {
+        data() {
+                return {
+                    clbOcc: 0
+                }
+            },
 
-export default {
-    data() {
-            return {
-                clbOcc: 0
-            }
+        async mounted(){
+            const delay = ms => new Promise(res => setTimeout(res, ms));
+            await this.fetchCurrCount();
+            //deleted the auth user part cos occupancy is constant across all users
         },
 
-    async mounted(){
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-        await this.fetchCurrCount();
-        //deleted the auth user part cos occupancy is constant across all users
-    },
+        methods: {
+            async fetchCurrCount() {
+                let today = new Date().toISOString().split('T')[0]
+                let fulltime = new Date().toISOString().split('T')[1]
+                let time = fulltime.split(':')[0].toString() + "00"
 
-    methods: {
-        async fetchCurrCount() {
-            const docRefOccupancy = await doc(db, "occupancy")
-            let count = docRefOccupancy.time
-            this.clbOcc = count/800;
-            return {
-                clbOcc,
-            };
+                console.log(time)
+                console.log(today)
+                const docRefOccupancy = await doc(db, "2023-04-06", "Occupancy")
+                const occupancyByTime = await getDoc(docRefOccupancy)
+                
+                if (occupancyByTime.exists()) {
+                    let count = occupancyByTime.data() 
+                    this.clbOcc = 100*count[time]/800;
+                }
+                return {
+                };
+            },
         },
-    },
-};
+    };
 </script>
 
 

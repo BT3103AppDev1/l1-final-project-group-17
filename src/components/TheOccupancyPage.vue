0a28f593e9710f62e090b93e30125116710a819b
jwhhhh <hor.jiaweii@gmail.com>
2023-04-05 22:52:30 +0800

    added real-time occupancy feature

--- src/components/TheOccupancyPage.vue
+++ src/components/TheOccupancyPage.vue
@@ -1,37 +1,51 @@
 <template>
-    <div id = "libraries">
-        <table id = "occupancytable">
-            <tr>
-                <th></th>
-                <th>Central Library</th>
-                <th>Chinese Library</th>
-            </tr>
-
-            <!--link to database and get occupancy. refer to profit display vue-->
-            <tr>
-                <td>Library occupancy</td>
-                <td>0.00%</td>
-                <td>0.00%</td>
-            </tr>
-        </table>
+    <div class = "occupancy">
+        <h2 id="Current occupancy"> CLB current occupancy: {{ clbOcc }}% </h2>
     </div>
 </template>
 
 <script>
-// to be completed 
+import firebaseApp from '../firebase.js';
+import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
+const db = getFirestore(firebaseApp);
+let today = new Date().toLocaleDateString().split('')[0];
+let fulltime = new Date().toLocaleDateString().split('')[1];
+let time = fulltime.split(':')[0] + '00';
+
+//get data from occupancy collection based on time 
+
+export default {
+    data() {
+            return {
+                clbOcc: 0
+            }
+        },
+
+    async mounted(){
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        await this.fetchCurrCount();
+        //deleted the auth user part cos occupancy is constant across all users
+    },
+
+    methods: {
+        async fetchCurrCount() {
+            const docRefOccupancy = await doc(db, "occupancy")
+            let count = docRefOccupancy.time
+            this.clbOcc = count/800;
+            return {
+                clbOcc,
+            };
+        },
+    },
+};
 </script>
 
 
 <style scoped>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-
-th,td {
-    border: 1px solid slategrey;
-    text-align: center;
-    padding: 8px;
+h1{
+    color: azure;
 }
 </style>

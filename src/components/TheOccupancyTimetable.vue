290f7bfaf2732be52ceca8f1d25a7aafd8750ab5
LINYEELEE <leelinyee@gmail.com>
2023-04-11 21:49:59 +0800

    edited occupancy timetable

--- src/components/TheOccupancyTimetable.vue
+++ src/components/TheOccupancyTimetable.vue
@@ -17,25 +17,7 @@
             <th>1900 - 2000</th>
             <th>2000 - 2100</th>
         </tr>
-        <tr v-for="(seat, index) in tableRows" :key="index">
-            <th>{{ seat }}</th>
-            <td id = "9am" :style="{backgroundColor: active1 ? 'indianred' : 'white'}"></td>
-            <td id = "10am" :style="{backgroundColor: active2 ? 'indianred' : 'white'}"></td>
-            <td id = "11am" :style="{backgroundColor: active3 ? 'indianred' : 'white'}"></td>
-            <td id = "12pm" :style="{backgroundColor: active4 ? 'indianred' : 'white'}"></td>
-            <td id = "1pm" :style="{backgroundColor: active5 ? 'indianred' : 'white'}"></td>
-            <td id = "2pm" :style="{backgroundColor: active6 ? 'indianred' : 'white'}"></td>
-            <td id = "3pm" :style="{backgroundColor: active7 ? 'indianred' : 'white'}"></td>
-            <td id = "4pm" :style="{backgroundColor: active8 ? 'indianred' : 'white'}"></td>
-            <td id = "5pm" :style="{backgroundColor: active9 ? 'indianred' : 'white'}"></td>
-            <td id = "6pm" :style="{backgroundColor: active10 ? 'indianred' : 'white'}"></td>
-            <td id = "7pm" :style="{backgroundColor: active11 ? 'indianred' : 'white'}"></td>
-            <td id = "8pm" :style="{backgroundColor: active12 ? 'indianred' : 'white'}"></td>
-        </tr>
     </table>
-    <div class = "test">
-            <button id = "testbutton" type = "button" v-on:click="loadtimetable">Load</button>
-    </div>
     <br><br>
 </template>
 
@@ -44,12 +26,11 @@
     import firebaseApp from'../firebase.js';
     import { FieldValue, getDocs, getFirestore } from "firebase/firestore";
     import { collection, getDoc, doc, deleteDoc, addDoc, setDoc, updateDoc } from "firebase/firestore";
-    import {getAuth, onAuthStateChanged} from "firebase/auth";
 
     const db = getFirestore(firebaseApp);
 
     export default {
-        mounted() {
+        async mounted() {
             this.library = this.$route.query.library
             this.level = this.$route.query.level
 
@@ -58,6 +39,8 @@
 
             //Nicer formatting of date to show users
             this.bookingdate2 = this.bookingdate.slice(-2) + "/" + this.bookingdate.slice(5, 7) + "/" + this.bookingdate.slice(0, 4)
+
+            await this.loadtimetable()
         },
 
         data(){
@@ -66,43 +49,112 @@
                 level: "",
                 bookingdate: "", //Default formatting of date for use in database
                 bookingdate2: "", //Nicer formatting of date to show users
-                tableRows: [],
-                active1: false,
-                active2: false,
-                active3: false,
-                active4: false,
-                active5: false,
-                active6: false,
-                active7: false,
-                active8: false,
-                active9: false,
-                active10: false,
-                active11: false,
-                active12: false,
             }
         },
 
         methods: {
-            loadtimetable() {
-                // let allDocuments = await getDocs(collection(db, String(bookingdate), String(library), String(level)));
+            async loadtimetable() {
+                console.log("Booking details: " + this.bookingdate + "|" + this.library + "|" + this.level)
+                let allDocuments = await getDocs(collection(db, String(this.bookingdate), String(this.library), String(this.level)))
+                var table = document.getElementById("table");
+                let seats = new Map()
 
-                for (let i = 1; i <= 332; i++) {
-                        this.tableRows.push("Seat " + i)
+                let tablelength = table.rows.length;
+                for (var i = tablelength - 1; i > 0; i--) {
+                    table.deleteRow(i)
+                }
+
+                allDocuments.forEach((docs) => {
+                    let timings = new Map()
+                    console.log("Seat number: ",docs.id)
+                    console.log("Booking data: ", docs.data())
+                    for (let key in docs.data()) {
+                        timings.set(key, 1)
                     }
+                    seats.set(docs.id, timings)
+                })
 
-                // if (library == "Central Library" && level == "Level 6") {
-                //     for (let i = 1; i <= 301; i++) {
-                //         this.tableRows.push("Seat " + i)
-                //     }
-                // }
+                console.log(seats)
 
-                // if (library == "Central Library" && level == "Level 6 (Chinese Library)") {
-                //     for (let i = 1; i <= 120; i++) {
-                //         this.tableRows.push("Seat " + i)
-                //     }
-                // }
+                let count = 332
+
+                if (this.library == "Central Library" && this.level == "Level 6") {
+                    count = 301
+                }
+
+                if (this.library == "Central Library" && this.level == "Level 6 (Chinese Library)") {
+                    count = 120
+                }                
+
+                for (let i = 1; i <= count; i++) {
+                    var row = table.insertRow();
+                    var cell1 = row.insertCell(0);
+					var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    var cell4 = row.insertCell(3);
+                    var cell5 = row.insertCell(4);
+                    var cell6 = row.insertCell(5);
+                    var cell7 = row.insertCell(6);
+                    var cell8 = row.insertCell(7);
+                    var cell9 = row.insertCell(8);
+                    var cell10 = row.insertCell(9);
+                    var cell11 = row.insertCell(10);
+                    var cell12 = row.insertCell(11);
+                    var cell13 = row.insertCell(12);
+                    cell1.innerHTML = "<b>Seat <b/>" + i;
+                    cell2.style.backgroundColor = "white"
+                    cell3.style.backgroundColor = "white"
+                    cell4.style.backgroundColor = "white"
+                    cell5.style.backgroundColor = "white"
+                    cell6.style.backgroundColor = "white"
+                    cell7.style.backgroundColor = "white"
+                    cell8.style.backgroundColor = "white"
+                    cell9.style.backgroundColor = "white"
+                    cell10.style.backgroundColor = "white"
+                    cell11.style.backgroundColor = "white"
+                    cell12.style.backgroundColor = "white"
+                    cell13.style.backgroundColor = "white"
+                    i = i.toString()
+                    if (seats.has(i)) {
+                        if (seats.get(i).has("0900")) {
+                            cell2.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1000")) {
+                            cell3.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1100")) {
+                            cell4.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1200")) {
+                            cell5.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1300")) {
+                            cell6.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1400")) {
+                            cell7.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1500")) {
+                            cell8.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1600")) {
+                            cell9.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1700")) {
+                            cell10.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1800")) {
+                            cell11.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("1900")) {
+                            cell12.style.backgroundColor = "indianred"
+                        }
+                        if (seats.get(i).has("2000")) {
+                            cell13.style.backgroundColor = "indianred"
+                        }
+                    }
+                }
             }
-
         }
     }
 </script>

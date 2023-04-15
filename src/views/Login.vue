b41ead75c8d5ec5d6b9041a0e5c477ccff5f0cbc
hyc-g <haoyaochen00@gmail.com>
2023-04-16 02:39:51 +0800

    change styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,16 +15,15 @@
 </script>
 
 <style scoped>
-header {
-    line-height: 1.5;
-}
+    header {
+        line-height: 1.5;
+    }
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
+    .logo {
+        display: block;
+        margin: 0 auto 2rem;
+    }
 
-@media (min-width: 1024px) {
     header {
         display: flex;
         place-items: center;
@@ -40,5 +39,9 @@ header {
         place-items: flex-start;
         flex-wrap: wrap;
     }
-}
+
+    html * {
+        font-family: Montserrat;
+    }
+
 </style>
